17ae254481620372f60e45715f8bcce3c7eea670
Kevin <kevc.dev@gmail.com>
2015-06-05 16:26:07 -0700

    Set up receipt section

--- js/jquery.js
+++ js/jquery.js
@@ -18,17 +18,29 @@ $(function() {
 			order.addPizza(newPizza);
 		});
 
-		$("p#total").text(order.total().toString());
+		$("#receipt").show();
+		$("#receipt .pizza-orders").html("");
+		$("#receipt").find("span#order-name").text(order.orderName);
+
+		order.pizzas.forEach(function(pizza, index){
+			$("#receipt .pizza-orders").append("<div class='pizza'>pizza " + (index +1).toString() + "<ul class='pizza-toppings'></ul></div>");
+
+			pizza.toppings.forEach(function(topping){
+				$("#receipt ul.pizza-toppings").last().append("<li>" + topping.ingredient + "</li>");
+			});
+		});
+
+		$("span#total").text(order.total().toString());
 	});
 
-	$("button#addPizza").on("click", function(event) {
+	$("button#btn-add-pizza").on("click", function(event) {
 		event.preventDefault();
 
 		var $createPizzaClone = $(".create-pizza").last().clone();
 		$createPizzaClone.find("input[type='checkbox']").prop("checked", false);
-		$createPizzaClone.find(".removePizza").show();
+		$createPizzaClone.find(".btn-remove-pizza").css("display", "inline-block");
 
-		$createPizzaClone.find(".removePizza").on("click", function() {
+		$createPizzaClone.find(".btn-remove-pizza").on("click", function() {
 			$(this).parent(".create-pizza").remove();
 		});
 		
