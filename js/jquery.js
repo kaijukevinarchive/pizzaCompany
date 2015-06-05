5cac2966d819c38c70ceddd360ee351fdf2543c2
Kevin <kevc.dev@gmail.com>
2015-06-05 14:25:48 -0700

    Fix exponentional cloning bug

--- js/jquery.js
+++ js/jquery.js
@@ -1,13 +1,15 @@
 $(function() {
 	$("form#order").submit(function(event) {
 		event.preventDefault();
+
 		var order = new Order($("input#name").val());
+
 		$(".create-pizza").each(function(){
 			var pizzaSize = $("select#size").val();
 			var checkedToppings = $(this).find("input[type='checkbox'][name='topping']:checked");
 			var myToppings = [];
 			checkedToppings.each(function() {
-				myToppings.push( new Topping( $(this).val(), parseFloat($(this).data("price"))) )
+				myToppings.push( new Topping( $(this).data("name"), parseFloat($(this).data("price"))) )
 			});
 			var newPizza = new Pizza(pizzaSize);
 			newPizza.setToppings(myToppings);
@@ -19,7 +21,9 @@ $(function() {
 
 	$("button#addPizza").on("click", function(event) {
 		event.preventDefault();
-		var $createPizzaClone = $(".create-pizza").clone();
-		$createPizzaClone.appendTo(".create-pizza-container")
+
+		var $createPizzaClone = $(".create-pizza").last().clone();
+		$createPizzaClone.find("input[type='checkbox']").prop("checked", false);
+		$createPizzaClone.appendTo(".create-pizza-container");
 	});
 })
