846f1f5abc199583f794fb8855848c4b6b905881
Kevin <kevc.dev@gmail.com>
2015-06-05 13:42:09 -0700

    Put all pizzas under one order

--- js/jquery.js
+++ js/jquery.js
@@ -1,21 +1,25 @@
 $(function() {
 	$("form#order").submit(function(event) {
 		event.preventDefault();
+		var order = new Order($("input#name").val());
+		$(".create-pizza").each(function(){
+			var pizzaSize = $("select#size").val();
+			var checkedToppings = $(this).find("input[type='checkbox'][name='topping']:checked");
+			var myToppings = [];
+			checkedToppings.each(function() {
+				myToppings.push( new Topping( $(this).val(), parseFloat($(this).data("price"))) )
+			});
+			var newPizza = new Pizza(pizzaSize);
+			newPizza.setToppings(myToppings);
+			order.addPizza(newPizza);
+		});
 
-		var pizzaSize = $("select#size").val();
-		var checkedToppings = $("input[type='checkbox'][name='topping']:checked");
-		var myToppings = [];
-		checkedToppings.each(function() {
-			myToppings.push( new Topping($(this).val(), parseFloat($(this).data("price"))) )
-		})
-		debugger
-		var newPizza = new Pizza(pizzaSize);
-		newPizza.setToppings(myToppings);
-		$("p#total").text(newPizza.price().toString());
+		$("p#total").text(order.total().toString());
 	});
 
 	$("button#addPizza").on("click", function(event) {
 		event.preventDefault();
-		$(".create-pizza").last().clone().appendTo(".create-pizza-container");
-	})
+		var $createPizzaClone = $(".create-pizza").clone();
+		$createPizzaClone.appendTo(".create-pizza-container")
+	});
 })
